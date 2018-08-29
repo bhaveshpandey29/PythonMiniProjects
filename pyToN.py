3f981ce312d3186462fc13e3641d80de31b64311
Bhavesh Pandey <bhavesh.hpandey@gmail.com>
2018-08-29 21:38:07 +0530

    py to n

--- pyToN.py
+++ pyToN.py
@@ -1,4 +1,6 @@
-#Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
-pi = (22/7)
-user_def_range = int(input("Please enter the range : "))
-print(f"here is the pi value till {user_def_range} digits:\n{round(pi,user_def_range)}")
+# #Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
+
+from mpmath import mp
+user_def_input = int(input("Please enter the digits you wish to see after decimals in Pi: "))
+mp.dps = user_def_input
+print(mp.pi)
